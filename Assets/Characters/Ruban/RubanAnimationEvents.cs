a2f370570c95b3e969c51436be49c77201c953a5
titaniumtank166 <rutgerkloppenburg@live.nl>
2023-04-09 19:28:50 +0200

    quick fix

--- Assets/Characters/Ruban/RubanAnimationEvents.cs
+++ Assets/Characters/Ruban/RubanAnimationEvents.cs
@@ -6,13 +6,15 @@ public class RubanAnimationEvents : MonoBehaviour
 {
     public GameObject  laserPrefab; 
     public Transform spwawnPoint; 
-
     public float LaserForce;
+
 
     public void RubanLaserbeamEvent()
     {
-        GameObject Laser = Instantiate(laserPrefab,spwawnPoint.position, spwawnPoint.rotation);
+        GameObject Laser = Instantiate(laserPrefab,spwawnPoint.position, Quaternion.identity);
         Rigidbody rb = laserPrefab.GetComponent<Rigidbody>();
-        rb.AddForce(spwawnPoint.forward * LaserForce, ForceMode.Impulse);
+        rb.velocity = (Vector3.forward*LaserForce);
+        //Destroy(laserPrefab,30f);
     }
+ 
 }
