ff35e8ac5571821bd1090f3c355fb994c777fe94
Soma Nyiro <nysoma@protonmail.com>
2023-04-13 16:37:30 +0200

    made arrows that point to players that are outside of the screen and can adapt their colors

---
+++ Assets/Scripts/PlayerColors.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColors
+{
+
+    public static Color[] colors =
+    {
+        Color.red,
+        Color.blue,
+        Color.yellow,
+        Color.green,
+        Color.magenta,
+        Color.cyan
+    };
+
+}
