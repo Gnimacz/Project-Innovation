1f2b428db3da13762ba6b4d1fa75a0e7af70977b
stefanov-1 <112878336+stefanov-1@users.noreply.github.com>
2023-04-15 17:27:08 +0200

    test1

--- Assets/Attacker.cs
+++ Assets/Attacker.cs
@@ -2,18 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using VisualEffects;
+
 
 public class Attacker : MonoBehaviour
 {
     public Animation animations;
-    public VisualEffect VFX_Hit;
-
-    public void DoVfx()
-    {
-        VFX_Hiz.Play();
-    }
-
+  
+    
     public void DoLightAttack()
     {
         animations.Play("DoLightAttack");
